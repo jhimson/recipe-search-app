20b2496e4707948412d465f539be02f9098fa0ea
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-07-05 22:41:15 -0400

    feat: search functionality

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Axios from 'axios';
 // ! Components
 import SearchBar from './SearchBar';
 // !-------------------------------->
 
-const Main = () => (
-  <div className="h-screen">
-    <SearchBar />
-  </div>
-);
-
+const Main = () => {
+  const [recipes, setRepices] = useState(null);
+  const submitSearch = async (word) => {
+    console.log(word);
+    const { data } = await Axios.get(``);
+  };
+  return (
+    <div className="h-screen">
+      <SearchBar submitSearch={submitSearch} />
+    </div>
+  );
+};
 export default Main;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,36 +1,48 @@
 /* eslint-disable react/button-has-type */
-import React from 'react';
+import React, { useState } from 'react';
 
-const SearchBar = () => (
-  <div className="flex items-center justify-center pt-5">
-    <div className="w-auto px-4 py-2 text-center bg-gray-200 border-2 border-gray-300 rounded-lg">
-      <form>
-        <div className="flex items-center justify-center space-x-2">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="p-2 border-2 rounded-lg outline-none focus:border-blue-300"
-          />
-          <button className="px-2 py-2 rounded-full focus:outline-none hover:bg-gray-300">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-7 h-7"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+const SearchBar = ({ submitSearch }) => {
+  const [searchBar, setSearchBar] = useState('');
+  return (
+    <div className="flex items-center justify-center pt-5">
+      <div className="w-auto px-4 py-2 text-center bg-gray-200 border-2 border-gray-300 rounded-lg">
+        <form>
+          <div className="flex items-center justify-center space-x-2">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchBar}
+              onChange={(e) => setSearchBar(e.target.value)}
+              className="p-2 border-2 rounded-lg outline-none focus:border-blue-300"
+            />
+            <button
+              type="button"
+              onClick={(e) => {
+                submitSearch(searchBar);
+                setSearchBar('');
+              }}
+              className="px-2 py-2 rounded-full focus:outline-none hover:bg-gray-300"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-        </div>
-      </form>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-7 h-7"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SearchBar;
