daf77a75287ddb60179a6cf476533f6e48014f18
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-07-06 06:37:43 -0400

    feat: api call for submitSearch

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 // ! Components
 import SearchBar from './SearchBar';
+import RecipeList from './RecipeList';
 // !-------------------------------->
 
 const Main = () => {
-  const [recipes, setRepices] = useState(null);
+  const [recipes, setRepices] = useState([]);
   const submitSearch = async (word) => {
     console.log(word);
-    const { data } = await Axios.get(``);
+    const { data } = await Axios.get(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${word}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}`
+    );
+    setRepices(data.hits);
   };
+
   return (
     <div className="h-screen">
       <SearchBar submitSearch={submitSearch} />
+      <RecipeList list={recipes} />
     </div>
   );
 };
