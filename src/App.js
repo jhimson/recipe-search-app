0afc74977d044bc7357d868588055dccc27e91e5
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-07-05 21:42:32 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// ! Components
+import Main from './components/Main';
+// !-------------------------------->
+
+const App = () => (
+  <div>
+    <Main />
+  </div>
+);
+
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main = () => (
+  <div className="h-screen">
+    <h1>Recipe app</h1>
+  </div>
+);
+
+export default Main;
